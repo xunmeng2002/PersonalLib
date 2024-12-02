53ba2206db64ab7e46f25f430a98cdfc1449ccdb
xunmeng2002 <150315803@qq.com>
2024-12-02 11:09:37 +0800

    从StepUtility中拆分StepHead

---
+++ Source/StepProtocol/StepHead.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+#include "Types.h"
+
+namespace step
+{
+	//报文头
+	class StepHead
+	{
+	public:
+		static constexpr UShortType FieldID = 0x0001;
+		UShortType PackageID;		//报文代码,报文唯一编号tag = 3固定长度5位
+		UShortType BodyLen;		//报文长度tag = 2固定长度5位
+		BoolType MessageChain;		//报文链标记tag = 4固定长度1位
+		IntType MsgSeqNum;		//请求编号tag = 5固定长度10位
+	};
+	//报文尾
+	class StepTail
+	{
+	public:
+		static constexpr UShortType FieldID = 0x0002;
+		UShortType CheckSum;		//校验和tag = 6固定长度3位
+	};
+}
--- Source/StepProtocol/StepUtility.h
+++ Source/StepProtocol/StepUtility.h
@@ -2,28 +2,13 @@
 #include <string>
 #include <unordered_map>
 #include "Types.h"
+#include "StepHead.h"
 
 namespace step
 {
 	constexpr unsigned int StepHeaderLen = 36u;
 	constexpr unsigned int StepTailLen = 7u;
 	constexpr unsigned int SOH = 1u;
-	class StepHead
-	{
-	public:
-		static constexpr UShortType FieldID = 0x0001;
-		UShortType PackageID;		//报文类别,报文唯一编号tag = 3固定长度5位
-		UShortType BodyLen;		//报文长度tag = 2固定长度5位
-		BoolType MessageChain;		//报文链标记tag = 4固定长度1位
-		IntType MsgSeqNum;		//请求编号tag = 5固定长度10位
-	};
-
-	class StepTail
-	{
-	public:
-		static constexpr UShortType FieldID = 0x0002;
-		UShortType CheckSum;		//校验和tag = 6固定长度3位
-	};
 
 
 	bool GetNextSoh(char* buff, int startIndex, int endIndex, int& sohIndex);
