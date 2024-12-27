21bca445295a305aca4a07fdd4f8589dcf1b169c
xunmeng2002 <150315803@qq.com>
2024-12-27 10:33:32 +0800

    1

---
+++ Source/DB/DBOperate.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Types.h"
+
+class DBOperate
+{
+public:
+	static DBOperate* Allocate();
+	virtual void Free() = 0;
+	virtual void FreeRecord() = 0;
+	virtual void FreeRecords() = 0;
+	virtual const char* GetDebugString() const = 0;
+
+
+	DBOperateType Operate;
+	unsigned int TableID;
+	unsigned int IndexID;
+	void* Record;
+};
