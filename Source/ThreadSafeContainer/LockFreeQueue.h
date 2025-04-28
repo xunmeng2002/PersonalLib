ddd053f864230ce6245e8118d0f1f55888324394
xunmeng2002 <150315803@qq.com>
2025-04-28 14:32:25 +0800

    Add LockFreeQueue

---
+++ Source/ThreadSafeContainer/LockFreeQueue.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <atomic>
+
+
+template <typename T>
+class LockFreeQueue
+{
+private:
+	struct Node
+	{
+		Node()
+			:data(nullptr), next(nullptr)
+		{ }
+		T* data;
+		Node* next;
+	};
+public:
+	LockFreeQueue()
+		:m_Head(new Node), m_Tail(m_Head.load())
+	{}
+	LockFreeQueue(const LockFreeQueue&) = delete;
+	LockFreeQueue& operator(const LockFreeQueue&) = delete;
+	~LockFreeQueue()
+	{
+		Node* oldHead = nullptr;
+		while (oldHead = m_Head.load())
+		{
+			m_Head.store(oldHead->next);
+			delete oldHead->data;
+			delete oldHead;
+		}
+	}
+
+	T* PopFront()
+	{
+		auto oldHead = m_Head.load();
+		if (oldHead == m_Tail.load())
+		{
+			return nullptr;
+		}
+		m_Head.store(oldHead->next);
+		auto data = oldHead->data;
+		delete oldHead;
+		return data;
+	}
+	void PushBack(T* data)
+	{
+		Node* node = new Node();
+		auto oldTail = m_Tail.load();
+		oldTail->data = data;
+		oldTail->next = node;
+		m_Tail.store(node);
+	}
+
+private:
+	std::atomic<Node*> m_Head;
+	std::atomic<Node*> m_Tail;
+};
+
