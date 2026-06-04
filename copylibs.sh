97031e2a67ddc8f18260f9260ffbcf4bb36d5c74
紫云 <150315803@qq.com>
2026-06-05 01:33:20 +0800

    1

--- copylibs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source $HOME/.profile
-
-sudo cp ./out/build/WSL-GCC-Debug/Target/PersonalLib/libPersonalLib* /usr/lib64
-cp ./out/build/WSL-GCC-Debug/Target/PersonalLib/libPersonalLib* ~/lib
-
