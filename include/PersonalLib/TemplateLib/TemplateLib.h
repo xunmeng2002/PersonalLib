aab76a059e4a5892c20451dff9ddbaf071c96ab4
紫云 <150315803@qq.com>
2026-06-08 01:25:38 +0800

    1

--- include/PersonalLib/TemplateLib/TemplateLib.h
+++ include/PersonalLib/TemplateLib/TemplateLib.h
@@ -4,6 +4,8 @@
 #include "PersonalLib/TemplateLib/Buffer/ShmBuffer.h"
 #include "PersonalLib/TemplateLib/ConfigStructs/ConfigStructs.h"
 #include "PersonalLib/TemplateLib/Constant/Constant.h"
+#include "PersonalLib/TemplateLib/DB/DBOperate.h"
+#include "PersonalLib/TemplateLib/DB/DBSubscriber.h"
 #include "PersonalLib/TemplateLib/MemCache/MemCacheTemplate.h"
 #include "PersonalLib/TemplateLib/MemCache/MemCacheTemplateSingleton.h"
 #include "PersonalLib/TemplateLib/ObjectPool/ObjectPool.h"
